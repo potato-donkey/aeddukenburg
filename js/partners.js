be5a7a2e2f460eb8a0d89268e9fcf7818c84b126
jxpsert <jasperdgp@outlook.com>
2023-04-01 11:59:52 +0200

    chore: add comments

--- js/partners.js
+++ js/partners.js
@@ -1,8 +1,10 @@
 const partnersList = document.querySelector("ul#partners");
 
 fetch("./data/partners.json").then((res) => {
+  // partners.json is a 2d array of [[name, url], [name, url], ...]
   res.json().then((data) => {
     data.forEach((partner) => {
+      // partner is an array of [name, url]
       const li = document.createElement("li");
       li.innerHTML = `
             <a href="${partner[1]}" class="has-text-white is-underlined" target="_blank">
